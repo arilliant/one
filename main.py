dd52a7fec52530651efd714e8888b8516d27ddab
Michael Demidov <arilliant@gmail.com>
2025-01-09 22:45:55 +0300

    Bot skeleton

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    pass
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from config import TOKEN
+
+
+dp = Dispatcher()
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
